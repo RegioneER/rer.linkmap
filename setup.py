64f92f8468db7514a4d0532f1f7b283959f9de9d
Federico Vancini <federico.vancini@redturtle.it>
2026-06-19 12:07:47 +0200

    lint

--- setup.py
+++ setup.py
@@ -13,4 +13,12 @@ except ImportError:
 
 
 if __name__ == "__main__":
-    setup()
+    setup(
+        classifiers=[
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
+            "Programming Language :: Python :: 3.13",
+        ],
+        python_requires=">=3.10,<3.14",
+    )
